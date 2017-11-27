2593f681e20c6c5fb460bcabf93145d3c58a0878
aaddo27 <aaddo27@gmail.com>
2017-11-27 15:49:16 -0500

    Update server.java

--- server.java
+++ server.java
@@ -8,46 +8,34 @@ import java.io.*;
  */
 
 public class Server {
-
+	
     public static void main(String[] args) throws Exception{
-
-		System.out.println("****Server****");
-		//opens a socket
-			ServerSocket ss = new ServerSocket(5327);
-			Socket cs = ss.accept();	//connection created
-
-			System.out.println("Connection Accepted: "+ ss);
-			//Open input and output stream
-			FileInputStream  in1 = new FileInputStream("E:\\s1.txt");
-			//FileInputStream  in2 = new FileInputStream("D:\\s2.txt");
-			//FileInputStream  in3 = new FileInputStream("D:\\s3.txt");
-			//FileInputStream  in4 = new FileInputStream("D:\\s4.txt");
-			byte b[] = new byte[50000];
-			in1.read(b,0,b.length);		//stores in b ranging from begining of byte to end
-			//in2.read(b,0,b.length);
-                        //in3.read(b,0,b.length);
-                        //in4.read(b,0,b.length);
-
-                        OutputStream out = cs.getOutputStream();
-                        out.write(b,0,b.length);
-
-			// BufferedReader br = new BufferedReader(new FileReader(" ");
-			// System.out.println(in.readUTF);
-
-			// System.out.println(out.writeUTF);
-
-			// //read and write to stream
-
-			// byte[] b = new byte[50];
-			// String s = b.readLine();
-			// BufferedReader br = new BufferedReader(new FileReader(" ");
-			// ods.writeUTF(s);	//write to stream
-			ss.close();
-			// System.out.println("The file has been transfered!");
-//
-//			catch(Exception er){
-//				er.printStackTrace();
-//			}
-//		}
-    }
+		
+        System.out.println("****Server****");
+        
+        try{
+            
+            ServerSocket ss = new ServerSocket(5354);            
+            Socket cs = ss.accept();	//connection created
+            System.out.println("Connection Accepted at: " + cs);
+            
+            //Open input stream
+            DataInputStream in = new DataInputStream(cs.getInputStream());  //receive data 
+            String s = in.readUTF();    //returns string
+            
+            FileOutputStream os1 = new FileOutputStream("D:\\t1.txt");
+            FileOutputStream os2 = new FileOutputStream("D:\\t2.txt");
+            FileOutputStream os3 = new FileOutputStream("D:\\t3.txt");
+            byte[] b = s.getBytes();
+           
+            os1.write(b, 0, b.length);
+            os2.write(b, 0, b.length);
+            os3.write(b, 0, b.length);
+            ss.close();
+            cs.close();
+         
+             }catch(IOException e ){
+                e.printStackTrace();     
+            			}
+                        }
 }
